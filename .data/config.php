55227ae704bb9c896b08954778fb08511ee19c8c
mylover520 <62501300+mylover520@users.noreply.github.com>
2022-08-05 06:42:06 +0000

    up

--- .data/config.php
+++ .data/config.php
@@ -1,17 +1,32 @@
 <?php $configs = '
 {
+    "": {
+        "shareapiurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIUdub09KMlpZdEV5VHRlSG84Y0ZKdm5EY1hnQ19ydTVKbDBiVHJfM25zSWFsTVJwTl80VG9SWkNSbUZEUkgzQzUvcm9vdA"
+    },
     "admin": "jgwy1234",
     "sitename": "5ZCs5p2-6ZiB",
     "theme": "classic.html",
     "timezone": "8",
-    "disktag": "Onedrive",
-    "Onedrive": {
+    "disktag": "kustphoto|kustphotox|kustmusic",
+    "kustphoto": {
         "Driver": "Sharelink",
-        "diskname": "T25lZHJpdmU",
-        "domainforproxy": "aHR0cHM6Ly9zdHVodWVsZWR1Y24uaXByb3h5LndvcmtlcnMuZGV2",
-        "public_path": "c2hhcmU",
-        "shareapiurl": "aHR0cHM6Ly9zdHVodWVsZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIUJ1Y2JabzY5YUUtb0RIZXRGUkdvRmV0NlpkaXd5WVZPczRGM21GYkFJeDA5bjNPOHNOQkVRSkpxbWVyUlo1M0kvcm9vdA",
-        "shareurl": "aHR0cHM6Ly9zdHVodWVsZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwva3lsZWhhb19zdHVfaHVlbF9lZHVfY24vRXBnQUhxdGQyQVJMcjFWSWY3cVN0WFVCYXBMMFpES3RlLXdRYXJLdzlhYjlqQT9lPXVBZmtzbw"
+        "diskname": "a3VzdHBob3Rv",
+        "domainforproxy": "a3VzdC42NjAwMDAueHl6",
+        "shareapiurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIXV6aUx4VVI3SGtLaHNZNTlGWDF4dF9MT2tVdDQwa3hKaTdtWGxwbmpBMF9rNFRDMW13QXVUN2Nfd2RTVlNvc3Ivcm9vdA",
+        "shareurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwvcGhvdG9fc3R1X2t1c3RfZWR1X2NuL0VncFdVcVJDa0MxSnBwc0dfRnFybFJVQmhGTVpYeUwwY01xb2NjZ0wyVlB3c3c_ZT1sNXJGbGc"
+    },
+    "kustphotox": {
+        "Driver": "Sharelink",
+        "diskname": "a3VzdHBob3RveA",
+        "domainforproxy": "a3VzdC42NjAwMDAueHl6",
+        "shareurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwvcGhvdG94X3N0dV9rdXN0X2VkdV9jbi9FbzkyMHNlNl9reEZwcjRzRF9pd0NJUUItSjhfeG9pYzhjSC0ydGtyb1A4dElRP2U9T2NDZ3hI"
+    },
+    "kustmusic": {
+        "Driver": "Sharelink",
+        "diskname": "a3VzdG11c2lj",
+        "domainforproxy": "a3VzdC42NjAwMDAueHl6",
+        "shareapiurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIUdub09KMlpZdEV5VHRlSG84Y0ZKdm5EY1hnQ19ydTVKbDBiVHJfM25zSWFsTVJwTl80VG9SWkNSbUZEUkgzQzUvcm9vdA",
+        "shareurl": "aHR0cHM6Ly9zdHVrdXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwvc2hhcmVfc3R1X2t1c3RfZWR1X2NuL0VzNWJQb19zM0wxSnBxWnNzblRoNklNQlF3TUFrX3AzLVdzOTBLSU5ZWEZQRmc_ZT1CclFlNHc"
     }
 }
 ';
