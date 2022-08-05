6d4dd30f1956126f9c83b448ee3104a29a4e4da9
mylover520 <62501300+mylover520@users.noreply.github.com>
2022-08-05 06:21:45 +0000

    up

---
+++ .data/config.php
@@ -0,0 +1,17 @@
+<?php $configs = '
+{
+    "admin": "jgwy1234",
+    "sitename": "5ZCs5p2-6ZiB",
+    "theme": "classic.html",
+    "timezone": "8",
+    "disktag": "Onedrive",
+    "Onedrive": {
+        "Driver": "Sharelink",
+        "diskname": "T25lZHJpdmU",
+        "domainforproxy": "aHR0cHM6Ly9zdHVodWVsZWR1Y24uaXByb3h5LndvcmtlcnMuZGV2",
+        "public_path": "c2hhcmU",
+        "shareapiurl": "aHR0cHM6Ly9zdHVodWVsZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vX2FwaS92Mi4wL2RyaXZlcy9iIUJ1Y2JabzY5YUUtb0RIZXRGUkdvRmV0NlpkaXd5WVZPczRGM21GYkFJeDA5bjNPOHNOQkVRSkpxbWVyUlo1M0kvcm9vdA",
+        "shareurl": "aHR0cHM6Ly9zdHVodWVsZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOmY6L2cvcGVyc29uYWwva3lsZWhhb19zdHVfaHVlbF9lZHVfY24vRXBnQUhxdGQyQVJMcjFWSWY3cVN0WFVCYXBMMFpES3RlLXdRYXJLdzlhYjlqQT9lPXVBZmtzbw"
+    }
+}
+';
